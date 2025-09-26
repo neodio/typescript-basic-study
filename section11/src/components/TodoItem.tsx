7635ef6e6bdabbb1b342d7b11031375584c62f3e
장정우 <jungwoo@lotte.net>
2025-09-26 17:38:55 +0900

    20250926 - 타입스크립트 - 리액트와 타입스크립트

---
+++ section11/src/components/TodoItem.tsx
@@ -0,0 +1,21 @@
+import {Todo} from "../type";
+import {useTodoDispatch} from "../App";
+
+interface Props extends Todo {
+}
+
+export default function TodoItem(props: Props) {
+
+    const dispatch = useTodoDispatch();
+
+    const onClickButton = () => {
+        dispatch.onClickDelete(props.id);
+    }
+
+    return (
+        <div>
+            {props.id}번 : {props.content}
+            <button onClick={onClickButton}>삭제</button>
+        </div>
+    );
+}
